408e9ee3650d863f4b1eb7af41da05c823d3d5bc
Jakob Eriksson <jakob@uic.edu>
2015-04-03 00:14:30 -0500

    added helloloop with signal handler

---
+++ helloloop.c
@@ -0,0 +1,15 @@
+#include "types.h"
+#include "user.h"
+
+void hello(int signum) {
+  printf(1,"Nuh-uh, I'm a non-preemptable hello loop!\n");
+}
+
+int main(int argc, char** argv) {
+  signal(2,hello);
+  while(1) {
+    printf(1,"Hello, still looping!\n");
+    sleep(100);
+  }
+}
+
