5690d990b336db90913a6422de0f1242c22034a6
Jakob Eriksson <jakob@uic.edu>
2016-09-28 21:41:32 -0700

    better dedup_writer

--- dedup_writer.c
+++ dedup_writer.c
@@ -5,7 +5,7 @@
 int
 main(int argc, char *argv[]) {
   printf(1,"Freepages at start: %d\n",freepages());
-  void* buf = malloc(10000000);
+  char* buf = malloc(10000000);
   memset(buf,0,10000000);
   printf(1,"Freepages after malloc: %d\n",freepages());
   dedup();
@@ -13,6 +13,20 @@ main(int argc, char *argv[]) {
   memset(buf,1,1000000);
   printf(1,"Freepages after writing a little: %d\n",freepages());
   dedup();
+
+  for(int i=0;i<1000000;i++) {
+    if(buf[i]!=1) {
+      printf(1,"Uh-oh! Found some junk in my frunk!\n");
+      exit();
+    }
+  }
+  for(int i=1000000;i<10000000;i++) {
+    if(buf[i]) {
+      printf(1,"Uh-oh! Found some junk in my trunk!\n");
+      exit();
+    }
+  }
+  
   printf(1,"Freepages after dedup: %d\n",freepages());
   memset(buf+1000000,1,9000000);  
   printf(1,"Freepages after writing the rest: %d\n",freepages());
