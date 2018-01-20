bc12760c191b19a9d3da768a602a7554196a0b0f
sam46 <abdlwahdsa@gmail.com>
2018-01-19 21:39:19 -0800

    meh

--- prettyprint.c
+++ prettyprint.c
@@ -1,4 +1,4 @@
-#include"types.h"
+#include "types.h"
 #include "user.h"
 
 char *hoarequote = "There are two ways of constructing a software design: One way is to make it so simple that there are obviously no deficiencies, and the other way is to make it so complicated that there are no obvious deficiencies. The first method is far more difficult.\n\n- C.A.R. Hoare (British computer scientist, winner of the 1980 Turing Award)\n\n";
